092f9902d55891f824866fab0aa0ce33b67cc365
voctif <victorgabriell1177@gmail.com>
2023-02-03 14:20:42 -0300

    componentes 1

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,13 @@
+import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
+import './components/basicos/primeiro'
+
 ReactDOM.render(
-  <div>ola react</div>,
+  <div>
+    <primeiro></primeiro>
+    a
+  </div>,
   document.getElementById('root')
 )
