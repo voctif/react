02a55271a098bd9f5fd1c5ce14d7654bd66b36cd
voctif <victorgabriell1177@gmail.com>
2023-02-03 23:34:25 -0300

    test component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 
-import './components/basicos/primeiro'
+import primeiro from './components/basicos/primeiro'
 
 ReactDOM.render(
   <div>
