9d81b5c3aa4358f5faeb3e5a153d41ef194d0a1f
voctif <victorgabriell1177@gmail.com>
2023-02-04 11:09:28 -0300

    yesterday erro

--- my-app/src/components/basicos/primeiro.js
+++ my-app/src/components/basicos/primeiro.js
@@ -1,3 +1,11 @@
+import React from "react"
+
 export default function Primeiro() {
-    return 'primeiro componente'
+    const msg = 'bem vindo'
+    return(
+    <div>
+        <h2>primeiro componente</h2>
+        <p>{ msg }</p>
+    </div>
+    )
 }
