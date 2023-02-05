f8f51bb40849d3a4ad0388ce125a67d28a5b4f85
voctif <victorgabriell1177@gmail.com>
2023-02-05 18:34:35 -0300

    fragmentos

---
+++ my-app/src/components/basicos/fragmento.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Fragmento(props){
+    return(
+        <React.Fragment>
+            <h2>cuidado com esse erro</h2>
+        </React.Fragment>
+    )
+}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,11 +4,13 @@ import React from 'react'
 
 import Primeiro from './components/basicos/primeiro'
 import ComParametro from './components/basicos/comParametro'
+import Fragmento from './components/basicos/fragmento'
 
 ReactDOM.render(
   <div>
     <Primeiro></Primeiro>
     <ComParametro titulo="parametro 2" aluno="victor" nota="10"></ComParametro>  
+    <Fragmento></Fragmento>
   </div>,
   document.getElementById('root')
 )
