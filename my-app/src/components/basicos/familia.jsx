d5ea5aa22d14d48c7c1b39b8ed41ba042af6e834
voctif <victorgabriell1177@gmail.com>
2023-02-21 15:32:32 -0300

    teste

--- my-app/src/components/basicos/familia.jsx
+++ my-app/src/components/basicos/familia.jsx
@@ -4,8 +4,7 @@ export default props => {
     return(
         <div>
             {
-            React.Children.map(props.children, child  => {
-                return cloneElement(child, props)})
+            React.Children.map(props.children, child  => {return cloneElement(child, props)})
             }
         </div>
 
