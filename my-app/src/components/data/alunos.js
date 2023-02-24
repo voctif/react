1620435d329f5bcc72072802fefc3ee26025842e
voctif <victorgabriell1177@gmail.com>
2023-02-24 09:39:12 -0300

    lista v5

---
+++ my-app/src/components/data/alunos.js
@@ -0,0 +1,11 @@
+export default [
+    {id: 1, nomne:'ana', nota: 9.2},
+    {id: 2, nomne:'bia', nota: 10.0},
+    {id: 3, nomne:'carlos', nota: 2.0},
+    {id: 4, nomne:'daniel', nota: 7.2},
+    {id: 5, nomne:'gui', nota: 8.2},
+    {id: 6, nomne:'rebeca', nota: 2.1},
+    {id: 7, nomne:'artur', nota: 1.0},
+    {id: 8, nomne:'pedro', nota: 4.5},
+    {id: 9, nomne:'gustavo', nota: 5.2},
+]
