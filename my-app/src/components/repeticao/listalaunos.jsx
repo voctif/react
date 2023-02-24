be034f4981c10ce7f3eb9b076660e736320ba529
voctif <victorgabriell1177@gmail.com>
2023-02-24 10:04:34 -0300

    lista fim

--- my-app/src/components/repeticao/listalaunos.jsx
+++ my-app/src/components/repeticao/listalaunos.jsx
@@ -2,25 +2,20 @@ import React from "react";
 import alunos from "../../data/alunos";
 
 export default (props) => {
-    const li1 = (
-        <li>
-            {alunos[0].id} {alunos[0].nome}  {alunos[0].nota}
-        </li>
-    );
-
+ 
     const lis = alunos.map((aluno) => {
-        return <li>
-            {alunos.id} {alunos.nome}  {alunos.nota}
-        </li>
-    })
+        return (
+            <li key={aluno.id}>
+                {aluno.id}  {aluno.nome}  {aluno.nota}
+            </li>
+        );
+    });
 
     return (
         <div>
             <ul>
-                <li>um</li>
-                <li>dois</li>
-                <li>tres</li>
-                <li>quatro</li>
+                <li>{lis}</li>
+
             </ul>
         </div>
     );
