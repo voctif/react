f1a98c67af05a9764b87c527740917649dca112c
voctif <victorgabriell1177@gmail.com>
2023-03-06 14:55:03 -0300

    teste

--- my-app/src/components/repeticao/tabelaprodutos.jsx
+++ my-app/src/components/repeticao/tabelaprodutos.jsx
@@ -3,19 +3,19 @@ import alunos from "../../data/produtos";
 
 export default (props) => {
  
-    const lis = alunos.map((produto) => {
+    const lis = produtos.map((produto) => {
         return (
             <li key={produto.id}>
-                {produto.id})  {produto.nome} - {produto.preco}
+                {produto.id} - {produto.nome} = R${produto.preco}
             </li>
         );
     });
+
 
     return (
         <div>
             <ul style={ {listStyle:"none"} }>
                 <li>{lis}</li>
-
             </ul>
         </div>
     );
--- my-app/src/data/produtos.js
+++ my-app/src/data/produtos.js
@@ -1,8 +1,8 @@
 export default [
-    { id: 1, nome: 'ana', preco: 9.2 },
-    { id: 2, nome: 'bia', preco: 10.0 },
-    { id: 3, nome: 'carlos', preco: 2.0 },
-    { id: 4, nome: 'daniel', preco: 7.2 },
-    { id: 5, nome: 'gui', preco: 8.2 },
+    { id: 1, nome: 'caneta', preco: 9.2 },
+    { id: 2, nome: 'lapis', preco: 10.0 },
+    { id: 3, nome: 'borracha', preco: 2.0 },
+    { id: 4, nome: 'papel', preco: 7.2 },
+    { id: 5, nome: 'garrafa', preco: 8.2 },
 
 ]
